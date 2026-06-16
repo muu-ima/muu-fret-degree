1dfaf4dada7f09dc155c4a408020740e7503997f
muu-ima <im777bass@gmail.com>
2026-06-16 15:01:00 +0900

    Upgrade Next.js for Vercel deployment

---
+++ app/not-found.tsx
@@ -0,0 +1,9 @@
+export default function NotFound() {
+  return (
+    <main className="notFoundPage">
+      <h1>ページが見つかりません</h1>
+      <p>URLを確認して、トップページからもう一度お試しください。</p>
+      <a href="/">トップページへ戻る</a>
+    </main>
+  );
+}
